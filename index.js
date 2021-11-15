255c5d99c3b8fe64bf17343440291ed9a42f05cb
Mason Newell <mason.newell13@gmail.com>
2021-11-14 16:03:53 -0800

    began script for readme generation

---
+++ index.js
@@ -0,0 +1,79 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const { listenerCount } = require("process");
+inquirer
+  .prompt([
+    {
+      name: "title",
+      message: "What is the name of the project?",
+      default: "default_title",
+    },
+    {
+      name: "description",
+      message: "Enter a description for the project",
+      default: "default_description",
+    },
+    {
+      name: "table_of_contents",
+      message: "Enter table of contents:  ",
+      default: "table_of_contents:",
+    },
+    {
+      name: "installation",
+      message: "Enter installation requirements",
+      default: "installation_requirements:",
+    },
+    {
+      name: "usage_information",
+      message: "Enter usage information",
+      default: "usage_information",
+    },
+    {
+      name: "contribution_guidelines",
+      message: "Enter contribution guidelines",
+      default: "contributing_guidelines",
+    },
+    {
+      name: "test_instructions",
+      message: "Enter test instructions",
+      default: "test_instructions",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license: ",
+      choices: ["MIT", "GPL", "Apache", "GNU", "N/A"],
+    },
+    {
+      name: "git_username",
+      message: "Enter GitHub username: ",
+    },
+    {
+      name: "email",
+      message: "Enter email: ",
+    },
+  ])
+  .then((data) => {
+    //  README setup
+    const readMeSetup = `# ${data.title}
+        ## Description: 
+        ${data.description}
+        ## Table of Contents:
+        ${data.table_of_contents}
+        ## Installation 
+        ${data.installation}
+        ## Usage Information:
+        ${data.usage_information}
+        ## Contribution Guidelines:
+        ${data.contribution_guidelines}
+        ## test_instructions:
+        ${data.test_instructions}
+        ### License: 
+        ${data.license}
+        # git_username: ${data.git_username}`;
+    // write to file
+    fs.writeFile("tester.md", readMeSetup, (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
